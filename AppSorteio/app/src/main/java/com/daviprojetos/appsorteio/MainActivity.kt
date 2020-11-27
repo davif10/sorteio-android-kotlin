3215527ab5f0fd82a398979e2b8185c8e19fc059
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2020-11-27 18:07:43 -0300

    Subindo o App
    Subindo o App

---
+++ AppSorteio/app/src/main/java/com/daviprojetos/appsorteio/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.daviprojetos.appsorteio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View){
+        var texto = findViewById<TextView>(R.id.textSorteio)
+        //texto.setText("Número sorteado é: ")
+        //var numeroSorteado = java.util.Random().nextInt(11)
+        var numeroSorteado = Random.nextInt(11)
+        texto.text = "Número sorteado é: " + numeroSorteado
+
+    }
+}
